07f94f0e3c68e174e1375c2e2d93be5d27815aa6
Eduardo Sebastián Lévano Cavero <79949221+lecav30@users.noreply.github.com>
2025-05-06 01:50:00 -0500

    feat: link to docs and menu mobile

---
+++ src/components/organisms/menu/index.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import { FaDownload, FaGithub } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+
+interface MenuProps {
+  show: boolean;
+  onClose: VoidFunction;
+}
+
+const Menu = ({ show, onClose }: MenuProps) => {
+  if (show)
+    return (
+      <div
+        className="bg-black/80 h-screen w-screen absolute -top-4 -left-4 sm:-left-8 flex 
+        justify-center items-center md:hidden"
+      >
+        <div
+          className="border border-white/60 relative w-10/12 min-h-8/12 backdrop-blur-lg
+          flex flex-col justify-center items-center gap-20 bg-white/20 rounded-3xl px-6 py-4"
+        >
+          <button
+            type="button"
+            onClick={onClose}
+            className="absolute top-8 right-8 cursor-pointer"
+          >
+            <IoClose size={20} />
+          </button>
+          <div className="flex flex-col items-center gap-5">
+            <Link
+              href="/"
+              onClick={onClose}
+            >
+              Home
+            </Link>
+            <Link
+              href="https://rapidwebforge-docs.netlify.app/"
+              target="_blank"
+              onClick={onClose}
+            >
+              Documentación
+            </Link>
+            <Link href="/contacto" onClick={onClose}>
+              Contacto
+            </Link>
+          </div>
+          <div className="flex gap-3 mx-auto">
+            <Link
+              href="https://github.com/RapidWebForge/RapidWebForgeApp"
+              target="_blank"
+              onClick={onClose}
+            >
+              <FaGithub scale={20} />
+            </Link>
+            <Link
+              href={`https://github.com/RapidWebForge/RapidWebForgeApp/releases/download/${process.env.NEXT_PUBLIC_INSTALLER_VERSION}/RapidWebForgeInstaller.exe`}
+              onClick={onClose}
+            >
+              <FaDownload scale={20} />
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Menu;
